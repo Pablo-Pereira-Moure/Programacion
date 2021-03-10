309ad5a0dd6664c3cd360d96d433bdad592b0601
Pablo <72071685+Pablo-Pereira-Moure@users.noreply.github.com>
2021-03-10 19:48:14 +0100

    Add files via upload

---
+++ Boletín 2/Boletin2_2/Boletin2_2.java
@@ -0,0 +1,18 @@
+
+package boletin2_2;
+
+
+public class Boletin2_2 {
+
+   
+    public static void main(String[] args) {
+       
+        float lado = 3;
+        float area;
+        area = lado*lado; 
+        System.out.println("Area="+area);
+
+    }
+    
+}
+
