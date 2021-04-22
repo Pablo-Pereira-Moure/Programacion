583589b64499bd0d7728925327665a3d2b6ce628
Pablo <72071685+ppereiramoure@users.noreply.github.com>
2021-04-22 12:46:31 +0200

    Boletín 21 subido

---
+++ src/com/3º evaluación/src/com/Pablopm07/Boletin21/Boletin21.java
@@ -0,0 +1,14 @@
+
+package com.Pablopm07.Boletin21;
+
+public class Boletin21 {
+
+    public static void main(String[] args) {
+        //Ventana 1
+       /* Ventana1 ventana1 = new Ventana1();
+        ventana1.setVisible(true);*/
+        //Ventana 2
+        Ventana2 ventana2 = new Ventana2();
+        ventana2.setVisible(true);
+    }
+}
