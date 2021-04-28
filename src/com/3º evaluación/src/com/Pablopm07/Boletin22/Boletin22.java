cfb71c9e20e04164217e18fad51a800fef358826
Pablo <72071685+ppereiramoure@users.noreply.github.com>
2021-04-28 12:22:42 +0200

    Boletin 22 subido

---
+++ src/com/3º evaluación/src/com/Pablopm07/Boletin22/Boletin22.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Pablopm07.Boletin22;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Boletin22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Ventana v=new Ventana();
+        v.setVisible(true);
+    }
+    
+}
