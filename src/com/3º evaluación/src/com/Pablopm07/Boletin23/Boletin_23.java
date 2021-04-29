dff8d3414bb7a8dd15516c95413ed23ef0cca922
Pablo <72071685+ppereiramoure@users.noreply.github.com>
2021-04-29 13:54:43 +0200

    Boletin 23 subido

---
+++ src/com/3º evaluación/src/com/Pablopm07/Boletin23/Boletin_23.java
@@ -0,0 +1,12 @@
+
+package com.Pablopm07.Boletin23;
+
+public class Boletin_23 {
+
+    public static void main(String[] args) {
+        Calculadora calculator = new Calculadora();
+        calculator.setResizable(false);
+        calculator.setVisible(true);
+    }
+    
+}
